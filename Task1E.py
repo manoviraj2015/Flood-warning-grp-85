fcd938b316f99dd8bbc04900f74d41c05452095f
Manoviraj Lal <ml2015@cam.ac.uk>
2022-02-02 18:09:11 +0000

    Task 1E and addition to geo

---
+++ Task1E.py
@@ -0,0 +1,10 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import rivers_by_station_number
+
+def run():
+    stations = build_station_list()
+    by_station_number = rivers_by_station_number(stations, 9)
+    print (by_station_number)
+
+if __name__ == "__main__":
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -15,3 +15,30 @@ def stations_within_radius(stations, centre,r):
         if haversine(centre, station.coord)<=r:
             station_radius_list.append(station)
     return station_radius_list
+
+
+def rivers_by_station_number(stations, N):
+    river_list=[]
+    for station in stations:
+        river_list.append(station.river)
+    
+    river_by_number=[]
+    for river in river_list:
+        if river not in [x[1] for x in river_by_number]:
+            number = river_list.count(river)
+            river_by_number.append((number, river))
+    
+    river_descending=sorted(river_by_number, reverse=True)
+
+    final_river_list=[]
+    i=0
+    while i<9:
+        final_river_list.append(river_descending[i])
+        i+=1
+    while river_descending[i][0]==river_descending[i-1][0]:
+        final_river_list.append(river_descending[i])
+        i+=1
+
+    final_river_list=[(t[1], t[0]) for t in final_river_list]
+
+    return final_river_list
