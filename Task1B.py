5f9ffde1cd555fb878dbe2aa9957a6c424b00b07
42tak <98126796+42tak@users.noreply.github.com>
2022-02-04 17:20:17 +0000

    Update Task1B.py

--- Task1B.py
+++ Task1B.py
@@ -1,1 +1,24 @@
+#tak42
+
+#from .utils import sorted_by_key  # noqa
+
+from floodsystem.stationdata import build_station_list
+from haversine import haversine, Unit
+from floodsystem.geo import stations_by_distance
+
+def run():
+
+  stations = build_station_list()
+
+  the_list = stations_by_distance(stations, (52.2053, 0.1218))
+
+  print("The closest 10 stations: ")           
+  print(the_list[:10])
+  print("The furthest 10 stations: ")           
+  print(the_list[-10:])
+
+
+if __name__ == "__main__":
+  print("**sorting stations by distance**")
+  run()
 
