1e45ae96e64b4ab72231ec755a0ce1897b4d6785
Manoviraj Lal <ml2015@cam.ac.uk>
2022-02-17 17:31:08 +0000

    Task 2C done

---
+++ Task2C.py
@@ -0,0 +1,15 @@
+#ml2015 
+
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    station_list=stations_highest_rel_level(stations, 10)
+    for station in station_list:
+        print(station.name, station.relative_water_level())
+
+if __name__ == "__main__":
+  run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -2,6 +2,7 @@
 flood data.
 
 """
+#ml2015
 
 def stations_level_over_threshold(stations,tol):
     
@@ -16,3 +17,23 @@ def stations_level_over_threshold(stations,tol):
     final_threshold_list = sorted(threshold_list, key=lambda x: x[1], reverse=True)
 
     return final_threshold_list
+
+
+def stations_highest_rel_level(stations, N):
+    list_highest_level = []
+    for station in stations:
+        m=station.relative_water_level()
+        if m==None:
+            pass
+        else:
+            list_highest_level.append((station,m))
+    
+    sorted_list_highest_level = sorted(list_highest_level, key=lambda x: x[1], reverse=True)
+
+    i=0
+    short_list=[]
+    while i<N:
+        short_list.append(sorted_list_highest_level[i][0])
+        i+=1
+
+    return short_list
