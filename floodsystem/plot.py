6e8286c57f3e714359536cb425715e9c5b348bfa
42tak <98126796+42tak@users.noreply.github.com>
2022-03-02 01:16:11 +0000

    Update plot.py
    don't hate me. This should be the last update

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,17 +1,43 @@
+#tak42
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+from floodsystem.analysis import polyfit
+from floodsystem.stationdata import build_station_list
 
-def plot_water_levels(station, dates, levels):
-
-    # Plot
+def plot_water_levels(station, dates, levels): 
+    
+    plt.axhline(y = station.typical_range[0])
+    plt.axhline(y = station.typical_range[1])
+    
     plt.plot(dates, levels)
 
-    # Add axis labels, rotate date labels and add plot title
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation=45);
-    plt.title("Station " + station)
+    plt.title(station.name)
 
-    # Display plot
-    plt.tight_layout()  # This makes sure plot does not cut off date labels
-
+    plt.tight_layout()  
     plt.show()
+   
+
+def plot_water_levels_with_fit(station, dates, levels, p): 
+    
+    float_dates = matplotlib.dates.date2num(dates)
+    poly, d = polyfit(dates, levels, p)
+    plt.plot(dates, poly(float_dates-d))
+
+    plt.plot(dates, levels)
+
+    plt.axhline(y = station.typical_range[0])
+    plt.axhline(y = station.typical_range[1])
+
+
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(station.name)
+
+    plt.tight_layout()  
+    plt.show()
+   
