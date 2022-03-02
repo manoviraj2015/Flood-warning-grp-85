ca3a3c26947b5bc9eac6e3ea69c91a8ec9fc8a74
42tak <98126796+42tak@users.noreply.github.com>
2022-03-02 03:03:37 +0000

    Update analysis.py

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -8,4 +8,39 @@ def polyfit(dates, levels, p):
     poly = np.poly1d(p_coeff)
     return poly, d0
 
+def floodwarning(station, dates, levels, p):
+    poly, d = polyfit(dates, levels, p)
+    poly_d1 = np.polyder(poly)      #first derivative of polynomial 
+    poly_d2 = np.polyder(poly_d1)   #second derivative of polynomial
+
+    high_value = station.typical_range[1]
+    low_value = station.typical_range[0]
+    latest_level = levels[0]
+    poly_d2_latest = poly_d2(0)
+
+    #setting default to be moderate
+    warning = 'moderate'
+
+    #severe when above highest level and increasing
+    if (latest_level > high_value and poly_d2_latest > 0):
+        warning = 'severe'
+    #high when above highest level but decreasing    
+    elif (latest_level > high_value and poly_d2_latest < 0):
+        warning = 'high'
+    #high when near highest level and increasing
+    elif (abs(latest_level - high_value) > abs(latest_level-low_value) and poly_d2_latest > 0):
+        warning = 'high'
+    #moderate when near highest level but decreasing
+    elif (abs(latest_level - high_value) > abs(latest_level-low_value) and poly_d2_latest < 0):   
+         warning = 'moderate'   
+    #moderate when near lower level and increasing
+    elif (abs(latest_level - high_value) < abs(latest_level-low_value) and poly_d2_latest > 0):  
+         warning = 'moderate'
+    #low when near lower level and decreasing
+    elif (abs(latest_level - high_value) < abs(latest_level-low_value) and poly_d2_latest < 0):
+         warning = 'low'
+    elif (latest_level < low_value and poly_d2_latest < 0):
+         warning = 'low'
+    
+    return warning
     
